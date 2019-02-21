1e8b38538d767a45f8991582d2c39e0d63d92ea7
zakiso <dzq1993@qq.com>
2019-02-21 16:22:24 +0800

    Initial Commit

---
+++ FlutterNativeiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlutterNativeiOS
+//
+//  Created by Zhiqiang Deng on 2019/2/21.
+//  Copyright © 2019 Zhiqiang Deng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
